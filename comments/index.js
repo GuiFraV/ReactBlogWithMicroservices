4cd16c27e01ff8fa00068a395c16e973fb18e8d6
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-03 12:12:00 +0100

    Add event publishing for comments and posts

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const { randomBytes } = require('crypto')
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 })
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async(req, res) => {
     const commentId = randomBytes(4).toString('hex'); // Je créer une string en hexadécimal de 4 bytes
     const { content } = req.body; // Je stock dans ma variable content le corps de la requête
 
@@ -24,6 +25,14 @@ app.post('/posts/:id/comments', (req, res) => {
 
     // Résultat : commentsByPostId{ 1: id: grgkegd7, 'Fist comment'; }
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
     res.status(201).send(comments);
 })
 
